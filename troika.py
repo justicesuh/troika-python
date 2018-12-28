9f1243b408766142c171a38257b1f52f35f74393
Justice Suh <justice.suh@gmail.com>
2018-12-28 13:56:34 -0500

    Add method stubs

---
+++ troika.py
@@ -0,0 +1,33 @@
+class Troika(object):
+    def __init__(self):
+        pass
+
+    def absort(self):
+        pass
+
+    def squeeze(self):
+        pass
+
+    def permutation(self):
+        pass
+
+    def sub_trytes(self):
+        pass
+
+    def shift_rows(self):
+        pass
+
+    def shift_lanes(self):
+        pass
+
+    def add_column_parity(self):
+        pass
+
+    def add_round_constant(self):
+        pass
+
+    def print_slice(self):
+        pass
+
+    def print_state(self):
+        pass
